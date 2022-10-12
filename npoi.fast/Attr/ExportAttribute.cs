a23db32b41411da86643b26950e3e606a216c732
BIG-HUIXIONG\HUA <crackerwork@petalmail.com>
2022-10-12 22:21:47 +0800

    完善

--- npoi.fast/Attr/ExportAttribute.cs
+++ npoi.fast/Attr/ExportAttribute.cs
@@ -4,22 +4,46 @@
  * @Last Modified by:   HUA 
  * @Last Modified time: 2022-10-07 11:13:22 
  */
+using NPOI.HSSF.Util;
 using NPOI.SS.UserModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace H.Npoi.Fast
 {
     /// <summary>
-    /// 导出设置，Xalign,Yalign作用于所有行，其余只对表头有效
+    /// 导出设置表头
     /// </summary>
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Property)]
     public class ExportAttribute : System.Attribute
     {
+        /// <summary>
+        /// 表头文字
+        /// </summary>
         private string? Name;
+        /// <summary>
+        /// 尺寸
+        /// </summary>
+        [Range(6,128)]
         public int FontSize = 14;
-        public bool Isbold = false;
-        public string Color = "#000000";
-        public string? BgColor;
+        /// <summary>
+        /// 是否加粗
+        /// </summary>
+        public bool Isbold = true;
+        /// <summary>
+        /// 文字颜色
+        /// </summary>
+        public short Color = HSSFColor.White.Index;
+        /// <summary>
+        /// 背景色
+        /// </summary>
+        public short BgColor = HSSFColor.SkyBlue.Index;
+        /// <summary>
+        /// 单元格水平对齐方式
+        /// </summary>
         public HorizontalAlignment Xalign = HorizontalAlignment.Center;
+        /// <summary>
+        /// 单元格垂直对齐方式
+        /// </summary>
         public VerticalAlignment Yalign = VerticalAlignment.Center;
 
         public ExportAttribute()
--- npoi.fast/Model/BaseExport.cs
+++ npoi.fast/Model/BaseExport.cs
@@ -16,26 +16,45 @@ using System.Threading.Tasks;
 
 namespace H.Npoi.Fast
 {
-    public class BaseExport<T>where T:class
+    public sealed class BaseExport<T>where T:class
     {
         public void Do(T entity)
         {
             HSSFWorkbook wb = new HSSFWorkbook();
             ISheet sheet=wb.CreateSheet();
             List<ExportAttribute> attributes = ExcelUtils.EntityAttrs<T>();
-            IRow row = sheet.CreateRow(1);
+            IRow row = sheet.CreateRow(0);
             for (int i = 0; i < attributes.Count; i++)
             {
                 ExportAttribute attr =attributes[i];
                 ICellStyle headStyle = wb.CreateCellStyle();
                 headStyle.Alignment = attr.Xalign;
                 headStyle.VerticalAlignment = attr.Yalign;
-                ICell cell = row.CreateCell(i+1);
+                headStyle.FillForegroundColor =attr.BgColor;
+                headStyle.FillPattern = FillPattern.SolidForeground;
+                IFont font = wb.CreateFont();
+                font.Color = attr.Color;
+                font.IsBold = attr.Isbold;
+                font.FontHeightInPoints = attr.FontSize;
+                headStyle.SetFont(font);
+                ICell cell = row.CreateCell(i);
+                cell.CellStyle = headStyle;
+                cell.SetCellValue(attr.GetName());
+                sheet.SetColumnWidth(i,256*(attr.GetName()?.Length??0)*6);
             }
-            foreach(PropertyInfo item in typeof(T).GetProperties())
+            IRow content = sheet.CreateRow(1);
+            var props = typeof(T).GetProperties();
+            for (int i = 0; i < props.Length; i++)
             {
-                Console.WriteLine(item.GetValue(entity));
+                ICellStyle cellStyle = wb.CreateCellStyle();
+                cellStyle.VerticalAlignment=VerticalAlignment.Center;
+                cellStyle.Alignment = HorizontalAlignment.Center;
+                ICell cell = content.CreateCell(i);
+                cell.CellStyle = cellStyle;
+                cell.SetCellValue(props[i].GetValue(entity)?.ToString());
             }
+            using FileStream fs = new FileStream(@"D:\Upload\1.xls", FileMode.OpenOrCreate, FileAccess.Write);
+            wb.Write(fs);
         }
     }
 }
